2f9de68b8e12c29108dea0d4c2d363500fbbade8
Anthony Eden <anthonyeden@gmail.com>
2012-07-05 17:26:59 +0200

    quick sort and tree examples.

---
+++ quicksort.erl
@@ -0,0 +1,19 @@
+-module(quicksort).
+-export([quicksort/1, lc_quicksort/1]).
+
+quicksort([]) -> [];
+quicksort([Pivot|Rest]) ->
+  {Smaller, Larger} = partition(Pivot,Rest,[],[]),
+  quicksort(Smaller) ++ [Pivot] ++ quicksort(Larger).
+
+lc_quicksort([]) -> [];
+lc_quicksort([Pivot|Rest]) ->
+  lc_quicksort([Smaller || Smaller <- Rest, Smaller =< Pivot])
+  ++ [Pivot] ++
+  lc_quicksort([Larger || Larger <- Rest, Larger > Pivot]).
+
+partition(_, [], Smaller, Larger) -> {Smaller, Larger};
+partition(Pivot, [H|T], Smaller, Larger) ->
+  if H =< Pivot -> partition(Pivot, T, [H|Smaller], Larger);
+     H >  Pivot -> partition(Pivot, T, Smaller, [H|Larger])
+  end.
